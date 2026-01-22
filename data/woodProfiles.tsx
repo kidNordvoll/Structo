e03a4a5f714d7db5a37d72628816d85bc43e0627
kidNordvoll <stefan.lauritsen@gmail.com>
2026-01-22 23:21:21 +0100

    Update

---
+++ data/woodProfiles.tsx
@@ -0,0 +1,13 @@
+{
+    name: "Furu C14",
+    type: "Furu",
+    grade: "C14",
+    properties: {
+        // Density, bending strength, modulus of elasticity, shear strength, etc.
+
+    },
+    availableSizes: ["48x48", "48x98", "48x148", "48x198",
+        "98x48", "98x98", "98x148", "98x198",
+        "148x48", "148x98", "148x148", "148x198",
+        "198x48", "198x98", "198x148", "198x198"]
+}
--- screens/home.tsx
+++ screens/home.tsx
@@ -1,46 +1,69 @@
 import { View, Text } from "react-native";
 import { useState } from "react";
 import { TextInput } from "react-native-paper";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
 
-    const [length, setLength] = useState(0);
-    const [width, setWidth] = useState(0);
-    const [centerCenter, onChangeCenterCenter] = useState('');
+    const [length, setLength] = useState("");
+    const [width, setWidth] = useState("");
+    const [centerCenter, setCenterCenter] = useState("60");
 
+    // Convert strings to numbers for calculations
+    const lengthNumber = parseFloat(length.replace(",", ".")) || 0;
+    const widthNumber = parseFloat(width.replace(",", ".")) || 0;
+
+    
     const handleLengthChange = (value: string) => {
 
-        const length = value === "" ? 0 : parseFloat(value);
-        setLength(length);
+        const periodCount = value.split(".").length - 1;
+        if (periodCount <= 1) {   
+            const cleaned = value.replace(/[^0-9.,]/g, "");
+            setLength(cleaned);
+        }
     }
 
     const handleWidthChange = (value: string) => {
-        const width = value === "" ? 0 : parseFloat(value);
-        setWidth(width);
+
+        const periodCount = value.split(".").length - 1;
+        if (periodCount <= 1) {   
+            const cleaned = value.replace(/[^0-9.,]/g, "");
+            setWidth(cleaned);
+        }
+    }
+
+    const handleCenterCenterChange = (value: string) => {
+        const cleaned = value.replace(/[^0-9.,]/g, "");
+        setCenterCenter(cleaned);
     }
 
     return (
-        <View>
-            <TextInput
-            style={{ height: 40, width: 120 }}
-            label="Length"
-            value={length.toString()} multiline={false} 
-            onChangeText={handleLengthChange} keyboardType="numeric" mode="outlined"/>
-            <TextInput
-            style={{ height: 40, width: 120 }}
-            label="Width"
-            value={width.toString()} multiline={false} 
-            onChangeText={handleWidthChange} keyboardType="decimal-pad" mode="outlined"/>
-            <TextInput
-            style={{ height: 40, width: 120 }}
-            label="C/C"
-            value={centerCenter} multiline={false}
-            onChangeText={onChangeCenterCenter} keyboardType="numeric" mode="outlined"/>
+        <SafeAreaProvider>
+            <SafeAreaView style={{ flex: 1, justifyContent: 'center'}}>
+                <TextInput
+                style={{ height: 40, width: 120 }}
+                label="Length"
+                value={length.toString()} multiline={false} 
+                onChangeText={handleLengthChange} keyboardType="numeric" mode="outlined"/>
+                <TextInput
+                style={{ height: 40, width: 120 }}
+                label="Width"
+                value={width.toString()} multiline={false} 
+                onChangeText={handleWidthChange} keyboardType="decimal-pad" mode="outlined"/>
+                <TextInput
+                style={{ height: 40, width: 120 }}
+                label="C/C"
+                value={centerCenter} multiline={false}
+                onChangeText={handleCenterCenterChange} keyboardType="numeric" mode="outlined"/>
 
-            <Text>Length: {length}</Text>
-            <Text>Width: {width}</Text>
-            <Text>Area: {length * width}</Text>
-        </View>
+                <Text>Length: {length}</Text>
+                <Text>Width: {width}</Text>
+                <Text>C/C: {centerCenter} cm</Text>
+                <Text>Number of joists: {Math.floor(lengthNumber / parseFloat(centerCenter)) + 1}</Text>
+                <Text>Area: {(lengthNumber * widthNumber) / 10000} m²</Text>
+            
+            </SafeAreaView>
+        </SafeAreaProvider>
     )
 }
 
